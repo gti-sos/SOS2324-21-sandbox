8c67baf3dd572663542da595d58838833b2d1d4d
Maria Luisa Rodriguez Cabrera <marrodcab12@alum.us.es>
2024-02-18 22:44:57 +0100

    prueba

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ let app = express();
 
 const PORT = (process.env.PORT || 23000);
 
-app.get('/hola', (req,res)=> {
-    res.send("<html><body><h1>Hello World</h1></body></html");
-});
+app.use("/static/", express.static("./public"));
 
 app.get('/cool', (req,res)=> {
-    res.send(`<html><body><h1>${cool}</h1></body></html`);
+    res.send(`<html><body><h1>${cool()}</h1></body></html`);
 });
 
-app.listen(PORT);
+app.listen(PORT, () =>{
+    console.log(`Server listening on port ${PORT}`)
+});
 
-console.log(`Server listening on port ${PORT}`);
+console.log(`Server inicializing...`);
